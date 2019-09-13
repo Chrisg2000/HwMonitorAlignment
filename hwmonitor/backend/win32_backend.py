f861500ce5b68a25824ee493c10d3d78f0c8e3b3
Chris <udo2000@freenet.de>
2019-09-14 01:05:57 +0200

    Connect new lib-style win32-api to backend

--- hwmonitor/backend/win32_backend.py
+++ hwmonitor/backend/win32_backend.py
@@ -1,11 +1,10 @@
 from hwmonitor.backend.monitor_backend import BaseMonitorBackend
-from hwmonitor.monitors.monitor import Monitor
-from hwmonitor.win32.flags import SystemMetricsFlags, QUERY_DEVICE_CONFIG_FLAGS, \
-    DISPLAYCONFIG_DEVICE_INFO_TYPE, DISPLAY_DEVICE_FLAGS, DEVICE_MODE_MODE, DEVICE_MODE_FIELD_FLAGS, \
-    CHANGE_DISPLAY_SETTINGS
-from hwmonitor.win32.func import GetSystemMetrics, EnumDisplaySettings, EnumDisplayDevices, GetDisplayConfigBufferSizes, \
-    QueryDisplayConfig, DisplayConfigGetDeviceInfo, ChangeDisplaySettingsEx
-from hwmonitor.win32.structs.displayconfig import DISPLAYCONFIG_TARGET_DEVICE_NAME
+from hwmonitor.monitors.monitor_factory import MonitorFactory
+from lib.win32.flags import ChangeDisplaySettings, DevmodeSettings, DevmodeFieldFlags, DisplayDeviceFlags, \
+    QueryDeviceConfigFlags, DisplayConfigDeviceInfoType, SystemMetricsFlags
+from lib.win32.func import GetSystemMetrics, ChangeDisplaySettingsEx, EnumDisplaySettings, GetDisplayConfigBufferSizes, \
+    QueryDisplayConfig, DisplayConfigGetDeviceInfo, EnumDisplayDevices
+from lib.win32.structs import DISPLAYCONFIG_TARGET_DEVICE_NAME
 
 
 class Win32Backend(BaseMonitorBackend):
@@ -32,16 +31,16 @@ class Win32Backend(BaseMonitorBackend):
         elif device_name not in self.monitor_model:
             raise ValueError(f"Monitor device {device_name} is not registered in this backend")
         devmode = EnumDisplaySettings(device_name,
-                                      DEVICE_MODE_MODE.ENUM_REGISTRY_SETTINGS)
+                                      DevmodeSettings.ENUM_REGISTRY_SETTINGS)
         devmode.dmPosition = (x, y)
-        devmode.dmFields = DEVICE_MODE_FIELD_FLAGS.POSITION  # only change the position of the screen
+        devmode.dmFields = DevmodeFieldFlags.POSITION  # only change the position of the screen
 
-        update_flags = CHANGE_DISPLAY_SETTINGS.UPDATEREGISTRY | CHANGE_DISPLAY_SETTINGS.GLOBAL
+        update_flags = ChangeDisplaySettings.UPDATEREGISTRY | ChangeDisplaySettings.GLOBAL
         if not reset:
             return ChangeDisplaySettingsEx(device_name,
                                            devmode,
                                            update_flags |
-                                           CHANGE_DISPLAY_SETTINGS.NORESET,
+                                           ChangeDisplaySettings.NORESET,
                                            None)
 
         return ChangeDisplaySettingsEx(device_name,
@@ -50,14 +49,14 @@ class Win32Backend(BaseMonitorBackend):
                                        None)
 
     def _discover_monitors(self):
-        path_count, mode_count = GetDisplayConfigBufferSizes(QUERY_DEVICE_CONFIG_FLAGS.ONLY_ACTIVE_PATHS)
-        display_paths, display_modes = QueryDisplayConfig(QUERY_DEVICE_CONFIG_FLAGS.ONLY_ACTIVE_PATHS, path_count,
+        path_count, mode_count = GetDisplayConfigBufferSizes(QueryDeviceConfigFlags.ONLY_ACTIVE_PATHS)
+        display_paths, display_modes = QueryDisplayConfig(QueryDeviceConfigFlags.ONLY_ACTIVE_PATHS, path_count,
                                                           mode_count)
 
         monitor_friendly_device_names = {}
         for path in display_paths:
             target_device_name = DISPLAYCONFIG_TARGET_DEVICE_NAME()
-            DisplayConfigGetDeviceInfo(target_device_name, DISPLAYCONFIG_DEVICE_INFO_TYPE.GET_TARGET_NAME,
+            DisplayConfigGetDeviceInfo(target_device_name, DisplayConfigDeviceInfoType.GET_TARGET_NAME,
                                        path.sourceInfo.adapterId, path.targetInfo.id)
             monitor_friendly_device_names[
                 target_device_name.monitorDevicePath] = target_device_name.monitorFriendlyDeviceName
@@ -71,10 +70,10 @@ class Win32Backend(BaseMonitorBackend):
                     try:
                         display_device_monitor = EnumDisplayDevices(display_device.DeviceName, dev_mon, 1)
 
-                        if display_device_monitor.StateFlags & DISPLAY_DEVICE_FLAGS.ATTACHED_TO_DESKTOP and \
-                                not display_device_monitor.StateFlags & DISPLAY_DEVICE_FLAGS.MIRRORING_DRIVER:
+                        if display_device_monitor.StateFlags & DisplayDeviceFlags.ATTACHED_TO_DESKTOP and \
+                                not display_device_monitor.StateFlags & DisplayDeviceFlags.MIRRORING_DRIVER:
                             devmode = EnumDisplaySettings(display_device.DeviceName,
-                                                          DEVICE_MODE_MODE.ENUM_REGISTRY_SETTINGS)
+                                                          DevmodeSettings.ENUM_REGISTRY_SETTINGS)
 
                             item = Monitor(
                                 device_name=display_device.DeviceName,
@@ -86,7 +85,7 @@ class Win32Backend(BaseMonitorBackend):
                                 position_x=devmode.DUMMYUNIONNAME.dmPosition.x,
                                 position_y=devmode.DUMMYUNIONNAME.dmPosition.y,
                                 orientation=devmode.DUMMYUNIONNAME.DUMMYSTRUCTNAME2.dmDisplayOrientation,
-                                primary=bool(display_device.StateFlags & DISPLAY_DEVICE_FLAGS.PRIMARY_DEVICE))
+                                primary=bool(display_device.StateFlags & DisplayDeviceFlags.PRIMARY_DEVICE))
 
                             self.monitor_model.add(item)
                         dev_mon += 1
